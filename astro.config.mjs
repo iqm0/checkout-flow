ca48689524a52da41efdd830abcd2ac20ffb835a
Igor Q Moreira <igorqm@gmail.com>
2025-01-31 16:41:46 +0000

    adding vercel support and renaming npm package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   vite: {
     envPrefix: 'SECRET_'
-  }
+  },
+
+  adapter: vercel()
 });
